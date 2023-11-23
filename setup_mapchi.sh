20ecb5e5381333ef8d8122a8fc44763c2ed6b086
Bluejuice1001 <bluejuice1001@gmail.com>
2023-11-23 14:23:46 +0200

    Update setup_mapchi.sh

--- setup_mapchi.sh
+++ setup_mapchi.sh
@@ -96,21 +96,31 @@ service nginx restart
 supervisorctl restart mapchicrm_django
 service nginx restart
 
-# Change default settings.py to settingsprod.py for manage.py
+# Change default settings.py to settingsprod.py, adds database columns for new changes
 cd /webapps/mapchi/environment_3_8_2/mapchecrm_django
 python manage.py makemigrations --settings mapchecrm_django.settingsprod
 python manage.py migrate --settings mapchecrm_django.settingsprod
 supervisorctl restart mapchicrm_django
 
 # Add compiled website front (First one live website, second one staging website)
-#sudo mv /root/dist /webapps/mapchi/dist
+sudo mv /root/setup/dist /webapps/mapchi/dist
+sudo mkdir /webapps/mapchi/dist/.well-known
+sudo mv /root/setup/apple-developer-merchantid-domain-association /webapps/mapchi/dist/.well-known
+sudo mv /root/setup/iframe.html /webapps/mapchi/dist/
+sudo mv /root/setup/widget.html /webapps/mapchi/dist/
+sudo mv /root/setup/loader.js /webapps/mapchi/dist/
+sudo mv /root/setup/loaderiframe.js /webapps/mapchi/dist/
+sudo mv /root/setup/loaderiframetest.js /webapps/mapchi/dist/
+sudo mv /root/setup/loaderwidget.js /webapps/mapchi/dist/
+sudo mv /root/setup/sitemap.xml /webapps/mapchi/dist/
+sudo mv /root/setup/robots.txt /webapps/mapchi/dist/
+
 #sudo mv /root /webapps/mapchi/staging_dist
 
 
 # Remove old files
 rm -fr /webapps/mapchi/environment_3_8_2/mapchecrm-main
 rm -fr /webapps/mapchi/req.txt
-rm -fr /root/setup_mapchi.sh
 
 # Output completion message
 echo "Mapchi setup completed successfully."
