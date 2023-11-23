cb6dd5a34e0836f41034d31bc32330cb3faaa702
Bluejuice1001 <bluejuice1001@gmail.com>
2023-11-23 15:35:26 +0200

    Update setup_mapchi.sh

--- setup_mapchi.sh
+++ setup_mapchi.sh
@@ -74,26 +74,14 @@ sudo mv /root/setup/mapchecrm_django.conf /etc/supervisor/conf.d/
 supervisorctl reread
 supervisorctl update
 
-# Get SSL certificate
-sudo certbot -d mapchi.com
-sudo certbot -d api.mapchi.com
-sudo certbot -d staging.mapchi.com
-sudo certbot -d www.mapchi.com
-
 # Setup Nginx
-sudo mv /root/setup/www.mapchi.com /etc/nginx/sites-available/
-sudo mv /root/setup/mapchi.com /etc/nginx/sites-available/
-sudo mv /root/setup/api.mapchi.com /etc/nginx/sites-available/
-sudo mv /root/setup/staging.mapchi.com /etc/nginx/sites-available/
+sudo mv /root/setup/mapchisetup.com /etc/nginx/sites-available/
+sudo mv /root/setup/api.mapchisetup.com /etc/nginx/sites-available/
 cd /etc/nginx/sites-enabled
-rm www.mapchi.com
-ln -s ../sites-available/www.mapchi.com .
-rm mapchi.com
-ln -s ../sites-available/mapchi.com .
-rm api.mapchi.com
-ln -s ../sites-available/api.mapchi.com .
-rm staging.mapchi.com
-ln -s ../sites-available/staging.mapchi.com .
+rm mapchisetup.com
+ln -s ../sites-available/mapchisetup.com .
+rm api.mapchisetup.com
+ln -s ../sites-available/api.mapchisetup.com .
 
 # Restart some services
 service nginx restart
