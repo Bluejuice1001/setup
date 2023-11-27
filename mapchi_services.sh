a57f98949a86d1601d3213a4e82b9aba3e2a4188
Bluejuice1001 <bluejuice1001@gmail.com>
2023-11-27 19:03:20 +0200

    Update mapchi_services.sh

--- mapchi_services.sh
+++ mapchi_services.sh
@@ -221,7 +221,26 @@ function restore_database() {
 
 function restore_latest_backup() {
     echo "Restoring the latest backup..."
-    # Add your steps to restore the latest backup here
+# Directory where backups are stored
+backup_dir="/webapps/mapchi/DB-Backup"
+
+# Find the latest backup file
+latest_backup=$(ls -t $backup_dir | head -n1)
+
+# Check if a backup file exists
+if [ -n "$latest_backup" ]; then
+    echo "Latest backup file found: $latest_backup"
+
+    # Full path to the latest backup file
+    backup_file="$backup_dir/$latest_backup"
+
+    # Restore the latest backup
+    PGPASSWORD=mapchipassword pg_restore -h localhost -U mapchiuser -d mapchi -Fc -c "$backup_file"
+
+    echo "Restore completed."
+else
+    echo "No backup files found in $backup_dir"
+fi
     echo "Latest backup restored successfully."
 }
 
