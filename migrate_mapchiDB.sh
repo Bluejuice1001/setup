3be3312f13e3b92379a4f5755b62fa10c89d2f10
Bluejuice1001 <bluejuice1001@gmail.com>
2023-11-27 17:32:07 +0200

    Update migrate_mapchiDB.sh

--- migrate_mapchiDB.sh
+++ migrate_mapchiDB.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-# Add new tables to postgres
+# Back-up clients database
 sudo mkdir /webapps/mapchi/DB-Backup
 current_date=$(date +"%Y%m%d_%H%M%S")
 backup_file="/webapps/mapchi/DB-Backup/backup_file_${current_date}.dump"
-pg_dump -h localhost -U mapchiuser -d mapchi -Fc -f "${backup_file}"
+PGPASSWORD=mapchipassword pg_dump -h localhost -U mapchiuser -d mapchi -Fc -f "${backup_file}"
+
+# Add new tables to postgres
 #cd /webapps/mapchi/environment_3_8_2/mapchecrm_django
 #python manage.py makemigrations --settings mapchecrm_django.settingsprod
 #python manage.py migrate --settings mapchecrm_django.settingsprod
